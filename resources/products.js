be4333485497ff4532117ae0d27620c41b8b2912
zahinafsar <afsarzahin@gmail.com>
2023-03-10 02:06:07 +0600

    Added product name instead of logo

--- resources/products.js
+++ resources/products.js
@@ -1,26 +1,31 @@
 export const products = [
   {
     image: "./apps/wecycle.png",
+    name: "WeCycle",
     width: 200,
     link: "https://wecycle.io/"
   },
   {
     image: "./apps/bapa.png",
+    name: "Nypdbapa",
     width: 100,
-    link: ""
+    link: "https://nypdbapa.org/"
   },
   {
     image: "./apps/arrayq.png",
+    name: "ArrayQ",
     width: 150,
     link: "https://arrayq.com/"
   },
   {
     image: "./apps/quint.png",
+    name: "Quint",
     width: 150,
-    link: "https://tanbelinc.github.io/Quint-Web/"
+    link: "https://quint.app/"
   },
   {
     image: "./apps/vp.jpeg",
+    name: "VenturePulse",
     width: 70,
     link: "https://www.venturepulsedaily.com/"
   }
